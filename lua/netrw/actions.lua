a4afc69de3a0fbe7b84cf05388f08a856c9dd198
Adam Figgins <adam@pop-os.localdomain>
2023-04-29 12:47:21 +0100

    fix(actions): Adds ability to use simple string mappings in config

--- lua/netrw/actions.lua
+++ lua/netrw/actions.lua
@@ -18,16 +18,20 @@ end
 ---@param bufnr number
 M.bind = function(bufnr)
     local opts = { noremap = true, silent = true }
-    for k, _ in pairs(config.options.mappings) do
-        -- Allow special characters in mapping key (i.e. "<leader>")
-        local escaped_key = k:gsub("([^%w])", "%%%1")
-        vim.api.nvim_buf_set_keymap(
-            bufnr,
-            "n",
-            k,
-            ':lua require"netrw.actions".dispatch("' .. escaped_key .. '")<cr>',
-            opts
-        )
+    for k, v in pairs(config.options.mappings) do
+        if type(v) == "string" then
+            vim.api.nvim_buf_set_keymap(bufnr, "n", k, v, opts)
+        else
+            -- Allow special characters in mapping key (i.e. "<leader>")
+            local escaped_key = k:gsub("([^%w])", "%%%1")
+            vim.api.nvim_buf_set_keymap(
+                bufnr,
+                "n",
+                k,
+                ':lua require"netrw.actions".dispatch("' .. escaped_key .. '")<cr>',
+                opts
+            )
+        end
     end
 end
 
